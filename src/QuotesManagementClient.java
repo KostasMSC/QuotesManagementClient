41e42eeff1ab908aac648c6407b16e797825ad9d
Kostas Argyris <konstantinos.argyris@intrasoft-intl.com>
2020-05-31 18:49:00 +0300

    test webhook

--- src/QuotesManagementClient.java
+++ src/QuotesManagementClient.java
@@ -12,8 +12,7 @@ public class QuotesManagementClient {
 		if (args.length == 0 || args[0].equals("-help")) {
 			help();
 		} 
-		/*
-		 * 
+		/* 
 		else if (args[0].equals("-stop")) {
 			if (args.length != 1) {
 				help();
